f931be97fc624b251cbb36ef2fc11c5b3473d231
vipin-kumar735 <patelharimohan31@gmail.com>
2025-07-25 20:53:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", '30DxNexGenBotsd6d3ca') # youtube song api ke
+API_KEY = getenv("API_KEY", '30DxNexGenBotse4e286') # youtube song api ke
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
