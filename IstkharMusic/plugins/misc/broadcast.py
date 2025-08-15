ebe5175f47e18550ec96d34536eca157f4f5a4a8
vipin-kumar735 <patelharimohan31@gmail.com>
2025-08-15 11:31:18 +0530

    Update broadcast.py

--- IstkharMusic/plugins/misc/broadcast.py
+++ IstkharMusic/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command("broadcast") & SUDOERS)
+@app.on_message(filters.command("broadcast") & Istkhar)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
@@ -167,6 +167,7 @@ async def auto_clean():
 
 
 asyncio.create_task(auto_clean())
+
 
 
 
